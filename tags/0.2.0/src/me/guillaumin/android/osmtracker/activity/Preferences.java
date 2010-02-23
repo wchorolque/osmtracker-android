afee2e772ec0f52aa50cc1df47ede8a81a1c69df
nguillaumin <nguillaumin@976c545a-1f70-11df-90ab-373db8be4c2b>
2010-02-23 23:06:17 +0000

    Bug corrections, minor UI changes, and first attempt to backgrounding the GPS logger service.

---
+++ tags/0.2.0/src/me/guillaumin/android/osmtracker/activity/Preferences.java
@@ -0,0 +1,50 @@
+package me.guillaumin.android.osmtracker.activity;
+
+import me.guillaumin.android.osmtracker.R;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
+import android.preference.Preference.OnPreferenceChangeListener;
+
+/**
+ * Manages preferences screen.
+ * 
+ * @author Nicolas Guillaumin
+ *
+ */
+public class Preferences extends PreferenceActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.xml.preferences);
+		
+		// Set summary of some preferences to their actual values
+		// and register a change listener to set again the summary in case of change
+		
+		// External storage directory
+		Preference pref = findPreference(OSMTracker.Preferences.KEY_STORAGE_DIR);
+		pref.setSummary(PreferenceManager.getDefaultSharedPreferences(this).getString(OSMTracker.Preferences.KEY_STORAGE_DIR, OSMTracker.Preferences.VAL_STORAGE_DIR));
+		pref.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
+			@Override
+			public boolean onPreferenceChange(Preference preference, Object newValue) {
+				preference.setSummary((String) newValue);
+				return true;
+			}
+		});
+
+		// Voice record duration
+		pref = findPreference(OSMTracker.Preferences.KEY_VOICEREC_DURATION);
+		pref.setSummary(PreferenceManager.getDefaultSharedPreferences(this).getString(OSMTracker.Preferences.KEY_VOICEREC_DURATION, OSMTracker.Preferences.VAL_VOICEREC_DURATION) + " " + getResources().getString(R.string.prefs_voicerec_duration_seconds));
+		pref.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
+			@Override
+			public boolean onPreferenceChange(Preference preference, Object newValue) {
+				preference.setSummary(newValue + " " + getResources().getString(R.string.prefs_voicerec_duration_seconds));
+				return true;
+			}
+		});		
+		
+	}
+	
+}
